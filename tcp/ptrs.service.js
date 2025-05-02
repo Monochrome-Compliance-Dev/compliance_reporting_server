df45e54723e0bf0b944a8b232b86ce19c6628151
DarryllRobinson <darryllrobinson@icloud.com>
2025-05-02 16:06:19 +1000

    feat: Introduce TCP-related functionality with new controller and service; update model attributes for consistency

---
+++ tcp/ptrs.service.js
@@ -0,0 +1,58 @@
+const db = require("../helpers/db");
+
+module.exports = {
+  getAll,
+  getAllByReportId,
+  getById,
+  create,
+  update,
+  delete: _delete,
+};
+
+async function getAll() {
+  return await db.Ptrs.findAll();
+}
+
+async function getAllByReportId(reportId) {
+  const ptrs = await db.Ptrs.findAll({
+    where: { reportId },
+  });
+  return ptrs;
+}
+
+async function getById(id) {
+  return await getPtrs(id);
+}
+
+async function create(params) {
+  // save ptrs
+  await db.Ptrs.create(params);
+}
+
+async function update(id, params) {
+  const ptrs = await getPtrs(id);
+
+  // validate
+  // if (
+  //   params.businessName !== ptrs.businessName &&
+  //   (await db.Ptrs.findOne({ where: { businessName: params.businessName } }))
+  // ) {
+  //   throw "Ptrs with this ABN already exists";
+  // }
+
+  // copy params to ptrs and save
+  Object.assign(ptrs, params);
+  await ptrs.save();
+}
+
+async function _delete(id) {
+  const ptrs = await getPtrs(id);
+  await ptrs.destroy();
+}
+
+// helper functions
+async function getPtrs(id) {
+  const ptrs = await db.Ptrs.findByPk(id);
+  if (!ptrs) throw { status: 404, message: "Ptrs not found" };
+  return ptrs;
+}
