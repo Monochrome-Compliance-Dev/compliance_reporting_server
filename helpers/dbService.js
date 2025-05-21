86677fcb26515b50be271510216f1af6725ba0cc
DarryllRobinson <darryllrobinson@icloud.com>
2025-05-21 22:41:57 +1000

    refactor: Replace static fallback for nanoid with dynamic import for better test handling

--- helpers/dbService.js
+++ helpers/dbService.js
@@ -2,13 +2,10 @@ const db = require("../helpers/db");
 const { logger } = require("../helpers/logger");
 
 let nanoid;
-
-try {
-  nanoid = require("nanoid").nanoid;
-} catch (err) {
-  // fallback during tests or unsupported environments
-  nanoid = () => "testid_" + Math.random().toString(36).substring(2, 10);
-}
+(async () => {
+  const { nanoid: importedNanoid } = await import("nanoid");
+  nanoid = importedNanoid;
+})();
 
 module.exports = {
   createRecord,
