fd560eac783fa578176f49d26ebadb6d25ac552e
DarryllRobinson <darryllrobinson@icloud.com>
2025-06-09 16:07:16 +1000

    fix: Update dotenv config to dynamically set environment file based on NODE_ENV

--- scripts/blogfaqinsert.js
+++ scripts/blogfaqinsert.js
@@ -1,6 +1,9 @@
 const path = require("path");
 require("dotenv").config({
-  path: path.resolve(__dirname, "../.env.development"),
+  path: path.resolve(
+    __dirname,
+    `../.env.${process.env.NODE_ENV || "development"}`
+  ),
 });
 const { Sequelize } = require("sequelize");
 const adminContentModel = require("../admin/admin.model"); // adjust if needed
