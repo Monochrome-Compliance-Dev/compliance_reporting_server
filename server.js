3aa694a89312fcb0981e614a1c63b46d5bf24b56
DarryllRobinson <darryllrobinson@icloud.com>
2025-06-30 11:48:10 +1000

    Fixing SSL false for testing

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const server = http.createServer(async (_, res) => {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   try {
